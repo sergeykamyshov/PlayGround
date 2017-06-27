a5b08125b0962bedb1f6c0f30b0e73f9076945e9
sergeykamyshov <kamishovsergey@gmail.com>
2017-06-27 08:56:23 +0300

    Added action (v1) for operations

--- SimpleCalculator/app/src/main/java/com/example/kamyshovcorp/simplecalculator/MainActivity.java
+++ SimpleCalculator/app/src/main/java/com/example/kamyshovcorp/simplecalculator/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.kamyshovcorp.simplecalculator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private List<String> basicOperations = Arrays.asList("+", "-", "*", "/");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +40,31 @@ public class MainActivity extends AppCompatActivity {
      */
     public void clearLastInput(View view) {
         TextView textViewResult = (TextView) findViewById(R.id.textViewResult);
-        String text = textViewResult.getText().toString();
-        if (!text.isEmpty()) {
-            textViewResult.setText(text.substring(0, text.length() - 1));
+        String result = textViewResult.getText().toString();
+        if (!result.isEmpty()) {
+            textViewResult.setText(result.substring(0, result.length() - 1));
+        }
+    }
+
+    public void countResult(View view) {
+        TextView textViewResult = (TextView) findViewById(R.id.textViewResult);
+        String result = textViewResult.getText().toString();
+
+        // You can not start input with operation
+        if (result.isEmpty()) {
+            return;
+        }
+
+        String lastSymbol = "";
+        if (!result.isEmpty()) {
+            lastSymbol = result.substring(result.length() - 1, result.length());
+        }
+        // If last symbol the operation then change the operation for new
+        if (basicOperations.contains(lastSymbol)) {
+            textViewResult.setText(result.substring(0, result.length() - 1));
+            textViewResult.setText(textViewResult.getText().toString() + ((Button) view).getText().toString());
+        } else {
+            textViewResult.setText(textViewResult.getText().toString() + ((Button) view).getText().toString());
         }
     }
 }
