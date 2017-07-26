f53449d06d2a78888e0f9c668e0e20440ce57a1d
sergeykamyshov <kamishovsergey@gmail.com>
2017-07-26 08:29:51 +0300

    Added emprty DrawerLayout and TopFragment

---
+++ SimpleTodo/app/src/main/java/com/kamyshovcorp/simpletodo/TopFragment.java
@@ -0,0 +1,17 @@
+package com.kamyshovcorp.simpletodo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TopFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_top, container, false);
+    }
+}
