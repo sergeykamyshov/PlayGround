2df59497a599d79c780cdedefede25fa7367bcdb
sergeykamyshov <kamishovsergey@gmail.com>
2017-07-28 08:28:37 +0300

    Added CoordinatorLayout and FAB

--- SimpleTodo/app/src/main/java/com/kamyshovcorp/simpletodo/TopFragment.java
+++ SimpleTodo/app/src/main/java/com/kamyshovcorp/simpletodo/TopFragment.java
@@ -2,12 +2,16 @@ package com.kamyshovcorp.simpletodo;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class TopFragment extends Fragment {
+
+    private FloatingActionButton mFab;
 
     public static TopFragment newInstance() {
         return new TopFragment();
@@ -16,6 +20,16 @@ public class TopFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_top, container, false);
+        View view = inflater.inflate(R.layout.fragment_top, container, false);
+
+        mFab = (FloatingActionButton) view.findViewById(R.id.fab);
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(getView(), "Test text", Snackbar.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
     }
 }
