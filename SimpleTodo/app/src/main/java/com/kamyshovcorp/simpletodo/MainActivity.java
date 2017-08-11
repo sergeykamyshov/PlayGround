573e3dd7bba3b9774211f52cfa1630848038836d
sergeykamyshov <kamishovsergey@gmail.com>
2017-08-11 12:17:38 +0300

    Added empty today list

--- SimpleTodo/app/src/main/java/com/kamyshovcorp/simpletodo/MainActivity.java
+++ SimpleTodo/app/src/main/java/com/kamyshovcorp/simpletodo/MainActivity.java
@@ -2,9 +2,12 @@ package com.kamyshovcorp.simpletodo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.kamyshovcorp.simpletodo.fragments.TodayListFragment;
 import com.kamyshovcorp.simpletodo.fragments.TopFragment;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,6 +30,30 @@ public class MainActivity extends AppCompatActivity {
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mDrawerCategories);
         mDrawerList.setAdapter(adapter);
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.content_frame, TopFragment.newInstance())
+                                .addToBackStack(null)
+                                .commit();
+                        break;
+                    case 1:
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.content_frame, TodayListFragment.newInstance())
+                                .addToBackStack(null)
+                                .commit();
+                        break;
+                    default:
+                        break;
+
+                }
+            }
+        });
     }
 
     public void showUpButton() {
---
+++ SimpleTodo/app/src/main/java/com/kamyshovcorp/simpletodo/fragments/TodayListFragment.java
@@ -0,0 +1,47 @@
+package com.kamyshovcorp.simpletodo.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.kamyshovcorp.simpletodo.MainActivity;
+import com.kamyshovcorp.simpletodo.R;
+
+public class TodayListFragment extends Fragment {
+
+    public static TodayListFragment newInstance() {
+        return new TodayListFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_list_today, container, false);
+        setEmptyImageBackground((CoordinatorLayout) view);
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        MainActivity activity = (MainActivity) getActivity();
+        if (activity != null) {
+            activity.hideUpButton();
+        }
+    }
+
+    private void setEmptyImageBackground(CoordinatorLayout view) {
+        ImageView imageView = new ImageView(getContext());
+        imageView.setImageResource(R.drawable.ic_empty_inbox_background_24dp);
+        imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+        CoordinatorLayout.LayoutParams layoutParams = new CoordinatorLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        imageView.setLayoutParams(layoutParams);
+
+        view.addView(imageView);
+    }
+}
