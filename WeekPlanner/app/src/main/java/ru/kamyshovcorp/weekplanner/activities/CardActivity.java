ca44255f7f5973c620e9821aafe7adaafd325d3a
sergeykamyshov <kamishovsergey@gmail.com>
2017-10-21 21:19:47 +0300

    Added filling CardActivity at creation

--- WeekPlanner/app/src/main/java/ru/kamyshovcorp/weekplanner/activities/CardActivity.java
+++ WeekPlanner/app/src/main/java/ru/kamyshovcorp/weekplanner/activities/CardActivity.java
@@ -4,10 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import ru.kamyshovcorp.weekplanner.R;
 import ru.kamyshovcorp.weekplanner.model.Card;
+import ru.kamyshovcorp.weekplanner.model.Task;
 
 public class CardActivity extends AppCompatActivity {
 
@@ -22,7 +27,40 @@ public class CardActivity extends AppCompatActivity {
         Bundle extras = intent.getExtras();
         Card card = extras.getParcelable(EXTRA_CARD);
 
+        fillCardTitle(card);
+        fillTasksList(card);
+    }
+
+    private void fillCardTitle(Card card) {
         EditText cardTitleEditText = (EditText) findViewById(R.id.card_title);
         cardTitleEditText.setText(card.getTitle());
+    }
+
+    private void fillTasksList(Card card) {
+        LinearLayout cardLayout = (LinearLayout) findViewById(R.id.card_layout);
+        for (Task task : card.getTasks()) {
+            createTaskItemLayout(cardLayout, task);
+        }
+    }
+
+    private void createTaskItemLayout(LinearLayout linearLayout, Task task) {
+        LinearLayout taskLayout = new LinearLayout(this);
+        LinearLayout.LayoutParams taskParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        taskLayout.setLayoutParams(taskParams);
+        taskLayout.setOrientation(LinearLayout.HORIZONTAL);
+
+        CheckBox taskCheckBox = new CheckBox(this);
+        taskCheckBox.setGravity(Gravity.TOP);
+        taskCheckBox.setChecked(task.isDone());
+        taskLayout.addView(taskCheckBox);
+
+        TextView taskDescriptionTextView = new TextView(this);
+        LinearLayout.LayoutParams taskDescriptionParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        taskDescriptionParams.gravity = Gravity.CENTER;
+        taskDescriptionTextView.setLayoutParams(taskDescriptionParams);
+        taskDescriptionTextView.setText(task.getDescription());
+        taskLayout.addView(taskDescriptionTextView);
+
+        linearLayout.addView(taskLayout);
     }
 }
