11b82c848ac45ae07dfe8153ce59d3639d6a6ea5
sergeykamyshov <kamishovsergey@gmail.com>
2017-11-08 22:37:07 +0300

    Added view for deleting a task

--- WeekPlanner/app/src/main/java/ru/kamyshovcorp/weekplanner/adapters/CardRecyclerAdapter.java
+++ WeekPlanner/app/src/main/java/ru/kamyshovcorp/weekplanner/adapters/CardRecyclerAdapter.java
@@ -5,11 +5,13 @@ import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import java.util.List;
 
@@ -33,30 +35,10 @@ public class CardRecyclerAdapter extends RecyclerView.Adapter<CardRecyclerAdapte
     }
 
     @Override
-    public void onBindViewHolder(final ViewHolder holder, int position) {
+    public void onBindViewHolder(ViewHolder holder, int position) {
         Task task = mTasks.get(position);
         holder.mIsDone.setChecked(task.isDone());
-        holder.mIsDone.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                mTasks.get(holder.getAdapterPosition()).setDone(isChecked);
-            }
-        });
         holder.mTaskDesc.setText(task.getDescription());
-        holder.mTaskDesc.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                mTasks.get(holder.getAdapterPosition()).setDescription(s.toString());
-            }
-        });
     }
 
     @Override
@@ -68,26 +50,74 @@ public class CardRecyclerAdapter extends RecyclerView.Adapter<CardRecyclerAdapte
 
         CheckBox mIsDone;
         EditText mTaskDesc;
+        ImageView mDeleteTask;
 
         public ViewHolder(View itemView) {
             super(itemView);
+
             mIsDone = itemView.findViewById(R.id.done_task_check_box);
+            mIsDone.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    mTasks.get(ViewHolder.this.getAdapterPosition()).setDone(isChecked);
+                }
+            });
+
             mTaskDesc = itemView.findViewById(R.id.description_task_edit_text);
+            // Обновляем данные после изменения описания задачи
+            mTaskDesc.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+                    mTasks.get(ViewHolder.this.getAdapterPosition()).setDescription(s.toString());
+                }
+            });
+            // Делаем описание задачи доступным для редактирования при касании
+            mTaskDesc.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    mTaskDesc.setFocusable(true);
+                    mTaskDesc.setFocusableInTouchMode(true);
+                    return false;
+                }
+            });
+            mDeleteTask = itemView.findViewById(R.id.img_more);
+            // Показываем/скрываем View для удаления задачи в зависимости от фокуса
+            mTaskDesc.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                @Override
+                public void onFocusChange(View v, boolean hasFocus) {
+                    if (hasFocus) {
+                        mDeleteTask.setVisibility(View.VISIBLE);
+                    } else {
+                        mDeleteTask.setVisibility(View.GONE);
+                    }
+                }
+            });
+
+            // Удаляем задачу
+            mDeleteTask.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    removeTask(ViewHolder.this.getAdapterPosition());
+                }
+            });
         }
-    }
-
-    public List<Task> getTasks() {
-        return mTasks;
-    }
-
-    public void setTasks(List<Task> tasks) {
-        tasks.clear();
-        mTasks.addAll(tasks);
-        notifyDataSetChanged();
     }
 
     public void addTask(Task task) {
         mTasks.add(task);
+        notifyDataSetChanged();
+    }
+
+    public void removeTask(int position) {
+        mTasks.remove(position);
         notifyDataSetChanged();
     }
 }
