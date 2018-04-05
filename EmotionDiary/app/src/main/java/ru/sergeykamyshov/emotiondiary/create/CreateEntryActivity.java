3484613452087f2971f37ce9561c0d8176deea5f
sergeykamyshov <kamishovsergey@gmail.com>
2018-04-05 15:41:58 +0300

    Created activity for adding new entry

---
+++ EmotionDiary/app/src/main/java/ru/sergeykamyshov/emotiondiary/create/CreateEntryActivity.java
@@ -0,0 +1,41 @@
+package ru.sergeykamyshov.emotiondiary.create;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import ru.sergeykamyshov.emotiondiary.R;
+
+public class CreateEntryActivity extends AppCompatActivity {
+
+    public static final boolean SHOW_HOME_AS_UP = true;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_entry);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(SHOW_HOME_AS_UP);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_create_entry, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+            case R.id.menu_action_save:
+                // save entry to database
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
--- EmotionDiary/app/src/main/java/ru/sergeykamyshov/emotiondiary/list/ListActivity.java
+++ EmotionDiary/app/src/main/java/ru/sergeykamyshov/emotiondiary/list/ListActivity.java
@@ -1,15 +1,19 @@
 package ru.sergeykamyshov.emotiondiary.list;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import ru.sergeykamyshov.emotiondiary.R;
+import ru.sergeykamyshov.emotiondiary.create.CreateEntryActivity;
 import ru.sergeykamyshov.emotiondiary.model.Event;
 
 public class ListActivity extends AppCompatActivity {
@@ -25,13 +29,23 @@ public class ListActivity extends AppCompatActivity {
         recylerView.setLayoutManager(new LinearLayoutManager(this));
         recylerView.setHasFixedSize(HAS_FIXED_SIZE);
         recylerView.setAdapter(new EventsRecyclerAdapter(this, generateTestEvents()));
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseContext(), CreateEntryActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private List<Event> generateTestEvents() {
         List<Event> events = new ArrayList<>();
         String empty = "";
         for (int i = 0; i < 50; i++) {
-            events.add(new Event(new Date(), "Situation " + i, empty, empty, empty));
+            events.add(new Event(new Date(), "Situation " + i + ". And long long long long " +
+                    "long long long long long long long long long long long ", empty, empty, empty));
         }
         return events;
     }
