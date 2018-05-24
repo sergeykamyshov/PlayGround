66de404660de15dd99aa66b72e8ac1a3760437bf
sergeykamyshov <kamishovsergey@gmail.com>
2018-05-24 23:03:41 +0300

    Changed colors and hide ActionBar

--- SplashScreen/app/src/main/java/ru/sergeykamyshov/splashscreen/ui/ui/splash/SplashActivity.java
+++ SplashScreen/app/src/main/java/ru/sergeykamyshov/splashscreen/ui/ui/splash/SplashActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -25,6 +26,11 @@ public class SplashActivity extends AppCompatActivity {
         if (!prefs.getBoolean(FIRST_LAUNCH_KEY, true)) {
             startMainActivity();
             finish();
+        }
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
         }
 
         setContentView(R.layout.activity_splash);
